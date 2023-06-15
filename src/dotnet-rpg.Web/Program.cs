680fa1d245f42ae767a85b17b388c6eac41d05b8
Roman Tikhonov <spooffit@mail.ru>
2023-06-15 17:05:11 +0300

    Injected DbContext to DI

--- src/dotnet-rpg.Web/Program.cs
+++ src/dotnet-rpg.Web/Program.cs
@@ -1,3 +1,6 @@
+using dotnet_rpg.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlite(builder.Configuration["DefaultConnection"]);
+});
 
 var app = builder.Build();
 
