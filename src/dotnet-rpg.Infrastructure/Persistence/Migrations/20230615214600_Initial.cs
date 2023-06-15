b738d132bd6f229db977bc0b953e44c27956d8e7
Roman Tikhonov <spooffit@mail.ru>
2023-06-16 00:46:55 +0300

    Added Initial Migration

---
+++ src/dotnet-rpg.Infrastructure/Persistence/Migrations/20230615214600_Initial.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace dotnet_rpg.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Character",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    HitPoints = table.Column<int>(type: "INTEGER", nullable: false),
+                    Strength = table.Column<int>(type: "INTEGER", nullable: false, defaultValue: 1),
+                    Defense = table.Column<int>(type: "INTEGER", nullable: false, defaultValue: 1),
+                    Intelligence = table.Column<int>(type: "INTEGER", nullable: false, defaultValue: 1),
+                    Class = table.Column<byte>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Character", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Character_Id",
+                table: "Character",
+                column: "Id",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Character");
+        }
+    }
+}
