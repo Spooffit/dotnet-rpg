b60b4a3e2767da4ebe39e53dc48cbef34f748e6d
Roman Tikhonov <spooffit@mail.ru>
2023-06-15 16:57:58 +0300

    Add DbInitializer.cs

---
+++ src/dotnet-rpg.Infrastructure/Persistence/DbInitializer.cs
@@ -0,0 +1,9 @@
+﻿namespace dotnet_rpg.Infrastructure.Persistence;
+
+public static class DbInitializer
+{
+    public static void Initialize(ApplicationDbContext context)
+    {
+        context.Database.EnsureCreated();
+    }
+}
