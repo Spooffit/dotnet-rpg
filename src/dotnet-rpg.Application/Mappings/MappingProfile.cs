0bf9aa68ebdfea16f2577626e792377de39b38de
Roman Tikhonov <spooffit@mail.ru>
2023-06-16 01:39:18 +0300

    Added AutoMapper

---
+++ src/dotnet-rpg.Application/Mappings/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using dotnet_rpg.Application.Dtos.Character;
+using dotnet_rpg.Core.Entities;
+
+namespace dotnet_rpg.Application.Mappings;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<Character, GetCharacterResponseDto>();
+        CreateMap<AddCharacterRequestDto, Character>();
+    }
+}
