ba8dbae9446534301d2a8bfe42cc0914910dd8c3
Roman Tikhonov <spooffit@mail.ru>
2023-06-15 20:39:01 +0300

    Added ServiceResponse.cs

---
+++ src/dotnet-rpg.Application/Services/ServiceResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace dotnet_rpg.Web.Services;
+
+public class ServiceResponse<T>
+{
+    public T? Data { get; set; }
+    public bool Success { get; set; } = true;
+    public string Message { get; set; } = string.Empty;
+}
