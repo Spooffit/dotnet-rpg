f59a9eff3a8d84e6e206eb9050c510817c5a3756
Roman Tikhonov <spooffit@mail.ru>
2023-06-15 16:34:49 +0300

    Added CharacterService.cs

---
+++ src/dotnet-rpg.Application/Services/ICharacterService.cs
@@ -0,0 +1,12 @@
+﻿using dotnet_rpg.Core.Entities;
+
+namespace dotnet_rpg.Application.Services;
+
+public interface ICharacterService
+{
+    Task<List<Character>> GetAllCharacters();
+    Task<Character> GetCharacterById(Guid id);
+    Task<List<Character>> AddCharacter(Character newCharacter);
+    Task<List<Character>> DeleteCharacter(Guid id);
+    
+}
---
+++ src/dotnet-rpg.Infrastructure/Services/CharacterService.cs
@@ -0,0 +1,41 @@
+﻿using dotnet_rpg.Application.Interfaces;
+using dotnet_rpg.Application.Services;
+using dotnet_rpg.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotnet_rpg.Infrastructure.Services;
+
+public class CharacterService : ICharacterService
+{
+    private readonly DbSet<Character> _dbSet;
+    
+    public CharacterService(
+        IApplicationDbContext context)
+    {
+        _dbSet = context.Character;
+    }
+    
+    public async Task<List<Character>> GetAllCharacters()
+    {
+        return await _dbSet.ToListAsync();
+    }
+
+    public async Task<Character> GetCharacterById(Guid id)
+    {
+        return await _dbSet.FindAsync(id);
+    }
+
+    public async Task<List<Character>> AddCharacter(Character newCharacter)
+    {
+        await _dbSet.AddAsync(newCharacter);
+        return await _dbSet.ToListAsync();
+    }
+
+    public async Task<List<Character>> DeleteCharacter(Guid id)
+    {
+        var entity = await _dbSet.FindAsync(id);
+        if (entity is not null)
+            _dbSet.Remove(entity);
+        return await _dbSet.ToListAsync();
+    }
+}
