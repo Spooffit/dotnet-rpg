428ef5e78921c94218108a1ae2cba227e316e9a7
Roman Tikhonov <spooffit@mail.ru>
2023-06-19 14:28:00 +0300

    Changed CharacterConfiguration.cs

--- src/dotnet-rpg.Infrastructure/Persistence/Configurations/CharacterConfiguration.cs
+++ src/dotnet-rpg.Infrastructure/Persistence/Configurations/CharacterConfiguration.cs
@@ -11,9 +11,10 @@ public class CharacterConfiguration : IEntityTypeConfiguration<Character>
         builder.HasKey(c => c.Id);
         builder.HasIndex(c => c.Id).IsUnique();
         builder.Property(c => c.Name).HasMaxLength(50).IsRequired();
-        builder.Property(c => c.Class).IsRequired();
+        builder.Property(c => c.HitPoints).HasDefaultValue(100);
         builder.Property(c => c.Defense).HasDefaultValue(1);
         builder.Property(c => c.Intelligence).HasDefaultValue(1);
         builder.Property(c => c.Strength).HasDefaultValue(1);
+        builder.Property(c => c.Class).IsRequired();
     }
 }
