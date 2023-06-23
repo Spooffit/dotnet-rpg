5218b39fa79efe8f7ef76194e590282eeaf171ad
Roman Tikhonov <spooffit@mail.ru>
2023-06-23 07:13:59 +0300

    Added CharacterRepository.cs

---
+++ src/dotnet-rpg.Application/Interfaces/Repositories/ICharacterRepository.cs
@@ -0,0 +1,12 @@
+﻿using dotnet_rpg.Core.Entities;
+
+namespace dotnet_rpg.Application.Interfaces.Repositories;
+
+public interface ICharacterRepository
+{
+    Task<List<Character?>> GetAllCharactersAsync();
+    Task<Character?> GetCharacterByIdAsync(Guid id);
+    Task<List<Character?>> AddCharacterAsync(Character? entity, CancellationToken cancellationToken = default);
+    Task<List<Character?>> DeleteCharacterByIdAsync(Guid id, CancellationToken cancellationToken = default);
+    Task<Character?> UpdateCharacterAsync(Character? entity, CancellationToken cancellationToken = default);
+}
---
+++ src/dotnet-rpg.Infrastructure/Persistence/Repositories/CharacterRepository.cs
@@ -0,0 +1,54 @@
+﻿using dotnet_rpg.Application.Interfaces.Repositories;
+using dotnet_rpg.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotnet_rpg.Infrastructure.Persistence.Repositories;
+
+public class CharacterRepository : ICharacterRepository
+{
+    private readonly ApplicationDbContext _db;
+    private readonly DbSet<Character> _dbSet;
+
+    public CharacterRepository(
+        ApplicationDbContext db)
+    {
+        _db = db;
+        _dbSet = _db.Set<Character>();
+    }
+    
+    public async Task<List<Character?>> GetAllCharactersAsync()
+    {
+        return await _dbSet.ToListAsync();
+    }
+
+    public async Task<Character?> GetCharacterByIdAsync(Guid id)
+    {
+        return await _dbSet.FindAsync(id);
+    }
+
+    public async Task<List<Character?>> AddCharacterAsync(Character? entity, CancellationToken cancellationToken = default)
+    {
+        await _dbSet.AddAsync(entity);
+        await _db.SaveChangesAsync(cancellationToken);
+
+        return await _dbSet.ToListAsync();
+    }
+
+    public async Task<List<Character?>> DeleteCharacterByIdAsync(Guid id, CancellationToken cancellationToken = default)
+    {
+        var entity = await _dbSet.FindAsync(id);
+        _dbSet.Remove(entity);
+
+        await _db.SaveChangesAsync(cancellationToken);
+        
+        return await _dbSet.ToListAsync();
+    }
+
+    public async Task<Character?> UpdateCharacterAsync(Character? entity, CancellationToken cancellationToken = default)
+    {
+        _dbSet.Update(entity);
+        await _db.SaveChangesAsync(cancellationToken);
+
+        return await _dbSet.FindAsync(entity);
+    }
+}
