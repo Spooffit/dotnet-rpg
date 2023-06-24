c985d3094958c9a69462677378630fa29ba6ad3c
Roman Tikhonov <spooffit@mail.ru>
2023-06-24 05:05:52 +0300

    Changed ServiceResponse.cs

--- src/dotnet-rpg.Application/Services/ServiceResponse.cs
+++ src/dotnet-rpg.Application/Services/ServiceResponse.cs
@@ -1,8 +1,6 @@
-﻿namespace dotnet_rpg.Web.Services;
+﻿namespace dotnet_rpg.Application.Services;
 
 public class ServiceResponse<T>
 {
     public T? Data { get; set; }
-    public bool Success { get; set; } = true;
-    public string Message { get; set; } = string.Empty;
 }
