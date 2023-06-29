4789cbdc85969687dba718bc9df52609c3ac7dea
Roman Tikhonov <spooffit@mail.ru>
2023-06-29 15:48:06 +0300

    Added MappingTests.cs

---
+++ tests/dotnet-rpg.Application.UnitTests/Mappings/MappingTests.cs
@@ -0,0 +1,48 @@
+﻿using System.Runtime.Serialization;
+using AutoMapper;
+using dotnet_rpg.Application.Dtos.Character;
+using dotnet_rpg.Application.Mappings;
+using dotnet_rpg.Core.Entities;
+
+namespace dotnet_rpg.Application.UnitTests.Mappings;
+
+public class MappingTests
+{
+    private readonly MapperConfiguration _configuration;
+    private readonly IMapper _mapper;
+
+    public MappingTests()
+    {
+        _configuration = new MapperConfiguration(cfg =>
+        {
+            cfg.AddMaps(typeof(MappingProfile));
+        });
+        _mapper = _configuration.CreateMapper();
+    }
+    
+    [Fact]
+    public void ShouldHaveValidConfiguration()
+    {
+        _configuration.AssertConfigurationIsValid();
+    }
+    
+    [Theory]
+    [InlineData(typeof(Character), typeof(GetCharacterResponseDto))]
+    [InlineData(typeof(AddCharacterRequestDto), typeof(Character))]
+    [InlineData(typeof(UpdateCharacterRequestDto), typeof(Character))]
+    [InlineData(typeof(UpdateCharacterRequestDto), typeof(GetCharacterResponseDto))]
+    public void ShouldSupportMappingFromSourceToDestination(Type source, Type destination)
+    {
+        var instance = GetInstanceOf(source);
+
+        _mapper.Map(instance, source, destination);
+    }
+    
+    private object GetInstanceOf(Type type)
+    {
+        if (type.GetConstructor(Type.EmptyTypes) != null)
+            return Activator.CreateInstance(type)!;
+
+        return FormatterServices.GetUninitializedObject(type);
+    }
+}
